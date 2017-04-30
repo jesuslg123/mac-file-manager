08a8eafd6de7324749e384ac879929d76ab36532
Jesus Lopez Garcia <jesuslg123@gmail.com>
2017-04-30 14:17:26 +0200

    Re-folder

---
+++ EmulatorFileManager/Emulator Manager/EmulatorManager.h
@@ -0,0 +1,13 @@
+//
+//  EmulatorManager.h
+//  EmulatorFileManager
+//
+//  Created by Jesus Lopez Garcia on 30/04/2017.
+//  Copyright © 2017 Jesus Lopez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmulatorManager : NSObject
+
+@end
---
+++ EmulatorFileManager/Emulator Manager/EmulatorManager.m
@@ -0,0 +1,13 @@
+//
+//  EmulatorManager.m
+//  EmulatorFileManager
+//
+//  Created by Jesus Lopez Garcia on 30/04/2017.
+//  Copyright © 2017 Jesus Lopez. All rights reserved.
+//
+
+#import "EmulatorManager.h"
+
+@implementation EmulatorManager
+
+@end
